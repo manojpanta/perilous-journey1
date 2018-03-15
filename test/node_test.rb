0b3c6440c14be4b175a66501e760d63248288ac2
Manoj Panta <manojpanta95@gmail.com>
2018-03-14 18:33:22 -0600

    initial commit

---
+++ test/node_test.rb
@@ -0,0 +1,32 @@
+require './lib/node'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+
+class NodeTest < Minitest::Test
+
+  def test_if_it_exists
+    node = Node.new("Burke")
+    assert_instance_of Node, node
+  end
+
+  def test_if_surname_works
+    node = Node.new("Burke")
+    assert_equal "Burke", node.surname
+  end
+
+  def test_for_next_node
+    node = Node.new("Burke")
+    assert_equal nil, node.next_node
+  end
+
+
+
+
+
+
+
+
+
+
+end
